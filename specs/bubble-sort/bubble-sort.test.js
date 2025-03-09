51b8b877963ca7a5ceea57f3b0ccd3c5490c652d
mussab-alpha <mussab.alphasquad@gmail.com>
2025-03-10 02:24:45 +0500

    completed/bubble-sort

--- specs/bubble-sort/bubble-sort.test.js
+++ specs/bubble-sort/bubble-sort.test.js
@@ -11,12 +11,26 @@
 */
 
 function bubbleSort(nums) {
+  let swapped = false;
+  for (let i = 0; i < nums.length - 1; i++) {
+    swapped = true;
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] > nums[j]) {
+        let temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+      }
+      swapped = true;
+    }
+    if (!swapped) break;
+  }
+  return nums;
   // code goes here
 }
 
 // unit tests
 // do not modify the below code
-test.skip("bubble sort", function () {
+test("bubble sort", function () {
   const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
   const sortedNums = bubbleSort(nums);
   expect(sortedNums).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
