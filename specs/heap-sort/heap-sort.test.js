c824e8db2845293ca0a172989d46dd810dd43d6c
mussab-alpha <mussab.alphasquad@gmail.com>
2025-03-12 14:33:29 +0500

    completed/heap-sort

--- specs/heap-sort/heap-sort.test.js
+++ specs/heap-sort/heap-sort.test.js
@@ -5,23 +5,49 @@
   You will probably need at least two more functions: heapify and createMaxHeap
    
 */
-
 const heapSort = (array) => {
-  // code
+  array = createMaxHeap(array);
+  for (let i = array.length - 1; i > 0; i--) {
+    swapPlace(0, i, array);
+    heapify(array, 0, i);
+  }
   return array;
 };
 
 const createMaxHeap = (array) => {
-  // code
+  //loop over array
+  for (let i = Math.floor(array.length / 2) - 1; i >= 0; i--) {
+    heapify(array, i, array.length);
+  }
+  return array;
+};
+
+const swapPlace = (indexA, indexB, array) => {
+  const temp = array[indexA];
+  array[indexA] = array[indexB];
+  array[indexB] = temp;
+  return array;
 };
 
 const heapify = (array, index, heapSize) => {
-  // code
+  const leftIndex = 2 * index + 1;
+  const rightIndex = 2 * index + 2;
+
+  let largestValueIndex = index;
+
+  if (leftIndex < heapSize && array[largestValueIndex] < array[leftIndex])
+    largestValueIndex = leftIndex;
+  if (rightIndex < heapSize && array[largestValueIndex] < array[rightIndex])
+    largestValueIndex = rightIndex;
+  if (largestValueIndex !== index) {
+    swapPlace(index, largestValueIndex, array);
+    heapify(array, largestValueIndex, heapSize);
+  }
 };
 
 // unit tests
 // do not modify the below code
-test.skip("heap sort", function () {
+test("heap sort", function () {
   const nums = [2, 5, 3, 8, 10, 6, 4, 7, 9, 1];
   heapSort(nums);
   expect(nums).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
